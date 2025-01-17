1fbd17fb404100f20f3b5b9dda0793ff356f034b
Paula Balcells <paula.balcells01@estudiant.upf.edu>
2025-01-17 21:20:17 +0100

    ci(pages): adds workflow file to deploy to github pages

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd());
 
   return {
-    base: env.VITE_BASE_URL || '',
+    base: env.VITE_BASE_URL || '/',
     plugins: [react(), tsconfigPaths()],
   };
 });
